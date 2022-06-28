d28b3340eb4270132f29f7db3ba85b6cf6797d4e
EdwinZhanCN <108310800+EdwinZhanCN@users.noreply.github.com>
2022-06-28 19:50:14 +0800

    Update world.java

--- world.java
+++ world.java
@@ -2,8 +2,8 @@ public class world {
     public static void main(String[] args) {
         Heroes Superman = new Heroes("Superman");
         Heroes Spiderman = new Heroes("Spiderman");
-
-        System.out.println(Superman.nFightsToTheDeath(Spiderman, 100));
+        
+        Batman.duelToTheDeath(Superman);
 
     }
 }
