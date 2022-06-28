fbf0c92d136725ef4cbc024e1f9a05dc36fde863
EdwinZhanCN <108310800+EdwinZhanCN@users.noreply.github.com>
2022-06-28 09:21:36 +0800

    Add files via upload

---
+++ Heroes.java
@@ -0,0 +1,75 @@
+public class Heroes {
+    private String name;
+    private int hitPoints;
+
+
+    public Heroes(String name){
+        this.name = name;
+        this.hitPoints = 100;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHitPoints(){
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints){
+        this.hitPoints = hitPoints;
+    }
+
+    public void duel(Heroes opponent){
+        double randomNumber = Math.random();
+        int damage = 0;
+        if( randomNumber < 0.5){
+            this.hitPoints -= 10;
+        }else{
+            damage = 10;
+        }
+        opponent.setHitPoints(opponent.getHitPoints() - damage);
+    }
+
+    public void duelToTheDeath(Heroes opponent){
+        while(this.hitPoints > 0 && opponent.getHitPoints() > 0){
+            duel(opponent);
+        }
+    }
+
+    public void phoenixDown(Heroes revival){
+        if(revival.getHitPoints() == 0) {
+            revival.setHitPoints(100);
+        }
+    }
+
+    public String nFightsToTheDeath (Heroes opponent, int times){
+        int winTimes = 0;
+        int opponentWinTimes = 0;
+        while (times > 0){
+            duelToTheDeath(opponent);
+            if(this.hitPoints == 0){
+                opponentWinTimes += 1;
+                phoenixDown(this);
+            }else {
+                winTimes += 1;
+                phoenixDown(opponent);
+            }
+            times -= 1;
+        }
+        return this.name + ": " + winTimes + " wins"+
+                "\n" + opponent.getName() + ": " + opponentWinTimes + " wins";
+    }
+
+
+
+
+    @Override
+    public String toString(){
+        return name + ": " + hitPoints + " hp";
+    }
+
+
+
+
+}
---
+++ world.java
@@ -0,0 +1,9 @@
+public class world {
+    public static void main(String[] args) {
+        Heroes Superman = new Heroes("Superman");
+        Heroes Spiderman = new Heroes("Spiderman");
+
+        System.out.println(Superman.nFightsToTheDeath(Spiderman, 100));
+
+    }
+}
