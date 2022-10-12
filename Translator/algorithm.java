295f5d27b95deec71b0b1d2716085b72a1544863
EdwinZhan <108310800+EdwinZhanCN@users.noreply.github.com>
2022-10-12 15:02:11 +0800

    update

--- Translator/algorithm.java
+++ Translator/algorithm.java
@@ -5,39 +5,44 @@ import java.util.*;
 public class algorithm {
     public static String translate(String target,ArrayList<Word> list){
         String[] tag = target.split(" ");
-        ArrayList<String> translation = new ArrayList<>();
+        String translation = "";
         String pinyin = "";
         int len = tag.length;
         ArrayList<Word> matches = new ArrayList<>();
+        ArrayList<Word> phrases = new ArrayList<>();
 
-        for (int j = 0; j < list.size(); j++) {
-            if(target.contains(list.get(j).getEnglish())) {
-                matches.add(list.get(j));
-            }
-        }
-        System.out.println(matches);
-        int pronCount = 0;
-        int vrbCount = 0;
-        int nonCount = 0;
-        String mw;
-        if(matches.size() > 1){
-            for(Word str: matches){
-                if(str.getPartOfSpeech().equalsIgnoreCase("pron")){
-                    translation.add(0,str.getChinese());
-                    pronCount++;
-                }else if(str.getPartOfSpeech().equalsIgnoreCase("v")){
-                    if(vrbCount < 1){translation.add(1,str.getChinese());}
-                    vrbCount++;
-                }else if(str.getPartOfSpeech().equalsIgnoreCase("n")){
-                    if(nonCount < 1){translation.add(str.getChinese());}
-                    nonCount++;
+        for (String s : tag) {
+            for (Word word : list) {
+                if (Objects.equals(s, word.getEnglish())) {
+                    matches.add(word);
                 }
             }
         }
 
-        String str =  delRepeat(String.join("",translation));
+
+        for (int j = 0; j < list.size(); j++) {
+            if(target.contains(list.get(j).getEnglish())) {
+                phrases.add(list.get(j));
+            }
+        }
+
+
+        for(Word match: matches){
+            int vrbCount = 0;
+            int nonCount = 0;
+            int adjCount = 0;
+            int mw = 0;
+            int pronCount = 0;
+            int advCount = 0;
+            translation += match.getChinese();
+        }
+
+        System.out.println(phrases);
+        System.out.println(matches);
+
+
         return pinyin +
-                "\n" + str;
+                "\n" + translation;
     }
 
 
