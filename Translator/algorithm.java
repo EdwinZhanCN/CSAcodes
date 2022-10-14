ad54a05cf66b0f2c37228cbf4c8ab13306dcc428
EdwinZhan <108310800+EdwinZhanCN@users.noreply.github.com>
2022-10-14 10:16:18 +0800

    update

--- Translator/algorithm.java
+++ Translator/algorithm.java
@@ -4,45 +4,65 @@ import java.util.*;
 
 public class algorithm {
     public static String translate(String target,ArrayList<Word> list){
+        String answer = "";
         String[] tag = target.split(" ");
-        String translation = "";
-        String pinyin = "";
         int len = tag.length;
-        ArrayList<Word> matches = new ArrayList<>();
+        ArrayList<String> matches = new ArrayList<>();
         ArrayList<Word> phrases = new ArrayList<>();
-
         for (String s : tag) {
             for (Word word : list) {
                 if (Objects.equals(s, word.getEnglish())) {
-                    matches.add(word);
+                    matches.add(word.getEnglish());
                 }
             }
         }
-
-
         for (int j = 0; j < list.size(); j++) {
             if(target.contains(list.get(j).getEnglish())) {
                 phrases.add(list.get(j));
             }
         }
-
-
-        for(Word match: matches){
-            int vrbCount = 0;
-            int nonCount = 0;
-            int adjCount = 0;
-            int mw = 0;
-            int pronCount = 0;
-            int advCount = 0;
-            translation += match.getChinese();
-        }
-
+        //
+        int vrbCount = 0;
+        int nonCount = 0;
+        int adjCount = 0;
+        int mw = 0;
+        int pronCount = 0;
+        int advCount = 0;
+        //
+        String[] translation = new String[tag.length];
+        int p1 = 0;
+        int p2 = 0;
         System.out.println(phrases);
-        System.out.println(matches);
-
-
-        return pinyin +
-                "\n" + translation;
+        System.out.println(Arrays.toString(tag));
+        try{
+            while(p1 < tag.length && p2 < phrases.size()){
+                if(phrases.get(p2).getEnglish().split(" ").length > 1){
+                    translation[IndexOf(tag, phrases.get(p2).getEnglish().substring(0,1))] =
+                            phrases.get(p2).getChinese();
+                    p1+=phrases.get(p2).getEnglish().split(" ").length;
+                }else if (phrases.get(p2).getEnglish().equals(tag[p1])) {
+                    translation[IndexOf(tag, tag[p1])] = phrases.get(p2).getChinese();
+                    p1++;
+                    p2 = 0;
+                    System.out.println(Arrays.toString(translation));
+                }else {
+                    p2++;
+                }
+            }
+        }catch (IndexOutOfBoundsException e){
+            for(int i = 0; i < translation.length; i++) {
+                if (translation[i] != null) {
+                    answer += translation[i];
+                }
+            }
+            return "The answer is: " + answer;
+        }
+        for(int i = 0; i < translation.length; i++){
+            if(translation[i] != null){
+                answer+=translation[i];
+            }
+        }
+        return "The answer is: " + answer;
     }
 
 
@@ -54,5 +74,14 @@ public class algorithm {
             }
         }
         return str;
+    }
+
+    public static int IndexOf(String[] array, String target){
+        for(int i = 0; i< array.length; i++){
+            if(array[i].equals(target)){
+                return i;
+            }
+        }
+        return -1;
     }
 }
--- out/production/CSAcodes/README.md
+++ out/production/CSAcodes/README.md
@@ -1,2 +1,12 @@
 # CSAcodes
 csa in-class code / Labratory
+if(phrases.get(p2).getPartOfSpeech().equals("v")){
+vrbCount++;
+if(vrbCount < 2){translation[IndexOf(tag,tag[p1])] = phrases.get(p2).getChinese();p1++;continue;}
+}else if(phrases.get(p2).getPartOfSpeech().equals("pron")){
+pronCount++;
+if(pronCount < 2){translation[IndexOf(tag,tag[p1])] = phrases.get(p2).getChinese();p1++;continue;}
+}else if(phrases.get(p2).getPartOfSpeech().equals("mw")){
+mw++;
+if(mw < 2){translation[IndexOf(tag,tag[p1])] = phrases.get(p2).getChinese();p1++;continue;}
+}
